4bbf771a88f4b3e70304513e9b6b2a92b111eb76
Jon <hp007hp31@gmail.com>
2016-06-14 17:49:04 -0400

    Added DB to commit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :static_pages
   resources :line_items
   resources :carts
   get 'store/index'
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
   root to: 'store#index', as: 'store'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get '/:id' => 'static_pages#show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
