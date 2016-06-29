7fd1fbdf49838efa8e61f02c846a45fb25315172
Jon <hp007hp31@gmail.com>
2016-06-28 20:40:41 -0400

    Added Spanish that doesnt work.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -66,7 +66,7 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
         format.html { redirect_to store_url, notice: 
-          'Thank you for your order.' }
+          I18n.t('.thanks') }
         format.json { render json: @order, status: :created,
           location: @order }
       else
